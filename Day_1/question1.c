31adcd5f3ee397b431a0d56a0f56a5b3f382fc5b
Madhav <sharmamaadhav22@gmail.com>
2026-05-28 16:44:20 +0530

    Day 1 uploaded

---
+++ Day_1/question1.c
@@ -0,0 +1,14 @@
+// quesiton 1
+ #include <stdio.h>
+
+int n;
+int sum = 0;
+
+ int main (){
+    printf("enter the value of n \n");
+    scanf("%d",&n);
+
+    for(int i = 1 ; i <= n ; i++ ){
+     sum = sum + i;
+ }
+ }
