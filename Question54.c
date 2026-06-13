d4a5fcd73b0499f69d1c3d39def91d25a4953668
madhav22-cpu <forkphantom94@gmail.com>
2026-06-13 13:17:46 +0530

    Day 14 Uploaded

---
+++ Question54.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, key, count = 0;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element: ");
+    scanf("%d", &key);
+
+    for(i = 0; i < n; i++) {
+        if(arr[i] == key)
+            count++;
+    }
+
+    printf("Frequency of %d = %d", key, count);
+
+    return 0;
+}
